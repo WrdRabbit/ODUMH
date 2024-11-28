dd2085fdac9a34b6df66175283e3400ae06168bf
WrdRabbit <isaaccar646@gmail.com>
2024-11-27 21:03:58 -0500

    minoritywavechange

--- minorityapp.js
+++ minorityapp.js
@@ -1,11 +1,11 @@
 // Fetch the resources from the minority-resources.json file
-fetch("minority-resources.json")
+fetch('minority-resources.json')
   .then(response => response.json())
   .then(data => {
     // Populate the resources for each community
     populateResources('black-community', data['black-community']);
-    populateResources('asian-community', data['asian-community']);
     populateResources('latinx-community', data['latinx-community']);
+    populateResources('asian-community', data['asian-community']);
     populateResources('indigenous-community', data['indigenous-community']);
   })
   .catch(error => console.error('Error loading minority resources:', error));
@@ -16,7 +16,7 @@ function populateResources(community, resources) {
   resources.forEach(resource => {
     const listItem = document.createElement('li');
     listItem.innerHTML = `
-      <a href="${resource.link}" target="_blank">${resource.name}</a>
+      <a href="${resource.link}" target="_blank"><strong>${resource.name}</strong></a>
       <p>${resource.description}</p>
     `;
     communityList.appendChild(listItem);
