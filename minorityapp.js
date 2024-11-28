041b06ab74615cf5a4654f079c371bb534d04f46
WrdRabbit <isaaccar646@gmail.com>
2024-11-28 13:34:56 -0500

    fix 34

--- minorityapp.js
+++ minorityapp.js
@@ -1,24 +1,32 @@
-// Fetch the resources from the minority-resources.json file
-fetch('minority-resources.json')
-  .then(response => response.json())
-  .then(data => {
-    // Populate the resources for each community
-    populateResources('black-community', data['black-community']);
-    populateResources('latinx-community', data['latinx-community']);
-    populateResources('asian-community', data['asian-community']);
-    populateResources('indigenous-community', data['indigenous-community']);
-  })
-  .catch(error => console.error('Error loading minority resources:', error));
+document.addEventListener("DOMContentLoaded", () => {
+  // Fetch the resources from the minority-resources.json file
+  fetch('minority-resources.json')
+      .then(response => response.json())
+      .then(data => {
+          // Populate the resources for each community
+          populateResources('black-resources-list', data['black-community']);
+          populateResources('latinx-resources-list', data['latinx-community']);
+          populateResources('asian-resources-list', data['asian-community']);
+          populateResources('indigenous-resources-list', data['indigenous-community']);
+      })
+      .catch(error => console.error('Error loading minority resources:', error));
+});
 
 // Function to populate the resources in the respective sections
-function populateResources(community, resources) {
-  const communityList = document.getElementById(`${community}-resources-list`);
+function populateResources(listId, resources) {
+  const communityList = document.getElementById(listId);
+
+  if (!communityList) {
+      console.error(`No element found with ID: ${listId}`);
+      return;
+  }
+
   resources.forEach(resource => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-      <a href="${resource.link}" target="_blank"><strong>${resource.name}</strong></a>
-      <p>${resource.description}</p>
-    `;
-    communityList.appendChild(listItem);
+      const listItem = document.createElement('li');
+      listItem.innerHTML = `
+          <a href="${resource.link}" target="_blank"><strong>${resource.name}</strong></a>
+          <p>${resource.description}</p>
+      `;
+      communityList.appendChild(listItem);
   });
 }
